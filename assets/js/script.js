65c8e2963f0b1ba9120e38f51eb2c8e8ba381de6
saif <saifalis@axioned.com>
2022-09-20 19:23:41 +0530

    equal to sign added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,17 @@
-// counter start
+// number counter function
 var countNum = document.querySelector('.counter');
 var loadingNum = document.querySelector('.loading-num');
 counter = 0;
 
 var counting = setInterval(runCounter, 17);
 function runCounter() {
-  if (counter != 100) {
+  if (counter !== 100) {
     loadingNum.innerHTML = counter++;
   } else {
     countNum.classList.add('fade-out-active');
     clearInterval(counting);
   }
 }
-// counter end
 
 
 
